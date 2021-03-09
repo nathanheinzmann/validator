6b99b796878c708ef2b303593654146922a3e0dc
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-08 21:33:37 -0300

    restore back to original files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Response } from './components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Response>Response</Response>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
