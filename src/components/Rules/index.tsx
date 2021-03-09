de7aff7ae53e2ba118d5d3a6a0e1181eed55b874
Nathan Heinzmann <nathanhph@gmail.com>
2021-03-09 11:13:44 -0300

    fix

--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { RulesText, Title, Wrapper } from './styles';
 
-
-const Rules = ({  }) => {
-  return(
-    <Wrapper>
-      <Title>Regras</Title>
-      <RulesText>Começar com vogal;</RulesText>
-      <RulesText>Letras permitidas: V, D, R e vogais;</RulesText>
-      <RulesText>Números são permitidos somente como último caractere;</RulesText>
-      <RulesText>Tamanho máximo: 4;</RulesText>
-
-
-    </Wrapper>
-  );
-}
+const Rules = () => (
+  <Wrapper>
+    <Title>Regras</Title>
+    <RulesText>Começar com vogal;</RulesText>
+    <RulesText>Letras permitidas: V, D, R e vogais;</RulesText>
+    <RulesText>Números são permitidos somente como último caractere;</RulesText>
+    <RulesText>Tamanho máximo: 4;</RulesText>
+  </Wrapper>
+);
 
 export default Rules;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Response } from './Response';
+export { default as Rules } from './Rules';
