43b37678c4461c20c53d1d06ce371a3ef1a62107
Nathan Heinzmann <nathanhph@gmail.com>
2021-03-10 12:59:41 -0300

    fixed the return of the function

--- src/utils/helpers/validator.ts
+++ src/utils/helpers/validator.ts
@@ -2,9 +2,7 @@ const validator = ( varName:string ):boolean => {
   
   const rule = /^[a, e, i, o, u][a, e, i, o , u, v, d, r]{0,2}[a, e, i, o, u, v, d, r, 0-9]{0,1}$/;
 
-  console.log(varName, "=", rule.test(varName) ? "Permitido" : "Não permitido");
-
-  return true;
+  return rule.test(varName);
 }
 
 export default validator;
