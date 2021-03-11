89c8d7759233fbbd40a2a3c9ec8a1993d644ee3e
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-11 10:18:38 -0300

    removed button to add fulltime scan

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,24 +1,18 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { Wrapper } from './styles';
 import ValueInsertedContext from '../../contexts/ValueInserted'
 
 
 const Input = () => {
-  const [localValue, setLocalValue] = useState("");
-  const { handleChangeValue } = useContext(ValueInsertedContext);
-
-  const handleClick = () => {
-    handleChangeValue(localValue)
-  };
+  const { handleChangeValue, value } = useContext(ValueInsertedContext);
 
   return(
     <Wrapper>
       <label><strong>Nome</strong></label>
       <input
-        onChange={({target})=> setLocalValue(target.value)} 
-        value={localValue} type="text"
+        onChange={({target})=> handleChangeValue(target.value)} 
+        value={value} type="text"
         placeholder="Nome da variável."/>
-      <button onClick={handleClick}>Testar</button>
     </Wrapper>
   );
 }
