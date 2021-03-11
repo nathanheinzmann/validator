e3d213c9cbc08dcba24f74e9bb4fa46c039771ee
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-03-11 00:45:55 -0300

    added context to handle value insertion

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-import { validator } from '../../utils';
+import { useContext, useState } from 'react';
 import { Wrapper } from './styles';
+import ValueInsertedContext from '../../contexts/ValueInserted'
 
 
 const Input = () => {
+  const [localValue, setLocalValue] = useState("");
+  const { handleChangeValue } = useContext(ValueInsertedContext);
 
-  const [varName, setVarName] = useState("exemplo");
-
-  const handleClick = () => { 
-    validator(varName);
-   }
+  const handleClick = () => {
+    handleChangeValue(localValue)
+  };
 
   return(
     <Wrapper>
       <label><strong>Nome</strong></label>
       <input
-        onChange={({target})=> setVarName(target.value)} 
-        value={varName} type="text"
+        onChange={({target})=> setLocalValue(target.value)} 
+        value={localValue} type="text"
         placeholder="Nome da variável."/>
       <button onClick={handleClick}>Testar</button>
     </Wrapper>
